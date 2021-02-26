71138d225e1eefca20bb013b70b5dac67d12f9a0
andrasbackman1 <63351159+andrasbackman1@users.noreply.github.com>
2021-02-26 11:50:11 +0100

    Update PlanetObj.py

--- PlanetObj.py
+++ PlanetObj.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+from math import atan, pi
 
 """
 Planet instances are objects intended to only store information about itself.
@@ -33,7 +34,7 @@ class Planet(object):
         Returns:
             None
         """
-
+        self.printed = False
         self.name = name
         with open("ParameterData.csv", "r") as file:
             reader = csv.reader(file)
@@ -43,6 +44,8 @@ class Planet(object):
                     self.position = np.array([row[2], row[3]], dtype = "float64")
                     self.velocity = np.array([row[4], row[5]], dtype = "float64")
                     self.acceleration = np.array([row[6], row[7]], dtype = "float64")
+        self.originalPosition = self.position.copy()
+        self.countSteps = 0
 
     def UpdateAcceleration(self, newAcceleration):
         """
@@ -59,7 +62,16 @@ class Planet(object):
         self.velocity = newVelocity
         return None
 
-    def UpdatePosition(self, newPosition):
+    def UpdatePosition(self, newPosition, timestep):
+        self.countSteps += 1
+        
+        theta = atan(newPosition[1]/newPosition[0])
+        if self.name != "Mars" and self.printed is False:
+            if theta >= 0 and theta <= 0.1 and self.countSteps > 5:
+                OrbPeriod = self.countSteps*(timestep[0])*1.15741*10**(-5)*2
+                print("{object}'s orbital period is: {period} days".format(object = self.name, period = round(OrbPeriod,4)))
+                self.printed = True  
+        
         self.position = newPosition
         return None
 
