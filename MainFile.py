59420f632b7db92aa4e27fe156e929eff79d7931
andrasbackman1 <63351159+andrasbackman1@users.noreply.github.com>
2021-02-26 11:53:50 +0100

    Update MainFile.py

--- MainFile.py
+++ MainFile.py
@@ -15,7 +15,6 @@ def main():
         Objects.append(Planet(object)) # Creates list of type planet object with a pointer
     
     Anim = animator()
-    simulator.Iterator(Objects, 300)
     Anim.run(Objects)
 
 if __name__ == "__main__":
